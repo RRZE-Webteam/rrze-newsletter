644a66dae4f9c72ae1f333c3769ea67c667d770a
rvdforst <rolf.v.d.forst@fau.de>
2021-04-04 18:09:36 +0200

    Update admin.js

--- dist/admin.js
+++ dist/admin.js
@@ -1,23 +1,2 @@
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************!*\
-  !*** ./src/admin/index.js ***!
-  \****************************/
-var jQuery = window && window.jQuery;
-jQuery(document).ready(function () {
-  jQuery(document).on("click", ".rrze-newsletter-activation-notice .notice-dismiss", function () {
-    var data = {
-      action: "rrze_newsletter_activation_notice_dismiss"
-    };
-
-    var _ref = window && window.rrze_newsletter_activation_nag_dismissal_params,
-        ajaxurl = _ref.ajaxurl;
-
-    jQuery.post(ajaxurl, data, function () {
-      return null;
-    });
-  });
-});
-/******/ })()
-;
+!function(){var n=window&&window.jQuery;n(document).ready((function(){n(document).on("click",".rrze-newsletter-activation-notice .notice-dismiss",(function(){var i=(window&&window.rrze_newsletter_activation_nag_dismissal_params).ajaxurl;n.post(i,{action:"rrze_newsletter_activation_notice_dismiss"},(function(){return null}))}))}))}();
 //# sourceMappingURL=admin.js.map
