227ef41feeed42cc33401ac3ca82b4844ac0b57d
rvdforst <rolf.v.d.forst@fau.de>
2021-04-13 13:53:18 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,108 +7,112 @@ const ESLintPlugin = require("eslint-webpack-plugin");
 const path = require("path");
 const admin = path.join(__dirname, "src", "admin");
 const editor = path.join(__dirname, "src", "editor");
+const subscription = path.join(__dirname, "src", "subscription");
 
 module.exports = (env, argv) => {
-  function isDevelopment() {
-    return argv.mode === "development";
-  }
-  var config = {
-    entry: {
-      admin,
-      editor,
-    },
-    output: {
-      filename: "[name].js",
-      clean: true,
-    },
-    optimization: {
-      minimizer: [
-        new TerserPlugin({ terserOptions: { sourceMap: true } }),
-        new OptimizeCSSAssetsPlugin({
-          cssProcessorOptions: {
-            map: {
-              inline: false,
-              annotation: true,
-            },
-          },
-        }),
-      ],
-    },
-    plugins: [
-      new MiniCSSExtractPlugin({
-        chunkFilename: "[id].css",
-        filename: (chunkData) => {
-          return "[name].css";
+    function isDevelopment() {
+        return argv.mode === "development";
+    }
+    var config = {
+        entry: {
+            admin,
+            editor,
+            subscription,
         },
-      }),
-      new ESLintPlugin(),
-    ],
-    devtool: isDevelopment() ? "cheap-module-source-map" : "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                plugins: ["@babel/plugin-proposal-class-properties"],
-                presets: [
-                  "@babel/preset-env",
-                  [
-                    "@babel/preset-react",
-                    {
-                      pragma: "wp.element.createElement",
-                      pragmaFrag: "wp.element.Fragment",
-                      development: isDevelopment(),
+        output: {
+            filename: "[name].js",
+            clean: true,
+        },
+        optimization: {
+            minimizer: [
+                new TerserPlugin({ terserOptions: { sourceMap: true } }),
+                new OptimizeCSSAssetsPlugin({
+                    cssProcessorOptions: {
+                        map: {
+                            inline: false,
+                            annotation: true,
+                        },
                     },
-                  ],
-                ],
-              },
-            },
-          ],
+                }),
+            ],
         },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            MiniCSSExtractPlugin.loader,
-            "css-loader",
-            {
-              loader: "postcss-loader",
-              options: {
-                postcssOptions: {
-                  plugins: [autoprefixer()],
+        plugins: [
+            new MiniCSSExtractPlugin({
+                chunkFilename: "[id].css",
+                filename: (chunkData) => {
+                    return "[name].css";
                 },
-              },
-            },
-            "sass-loader",
-          ],
+            }),
+            new ESLintPlugin(),
+        ],
+        devtool: isDevelopment() ? "cheap-module-source-map" : "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                plugins: [
+                                    "@babel/plugin-proposal-class-properties",
+                                ],
+                                presets: [
+                                    "@babel/preset-env",
+                                    [
+                                        "@babel/preset-react",
+                                        {
+                                            pragma: "wp.element.createElement",
+                                            pragmaFrag: "wp.element.Fragment",
+                                            development: isDevelopment(),
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        MiniCSSExtractPlugin.loader,
+                        "css-loader",
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [autoprefixer()],
+                                },
+                            },
+                        },
+                        "sass-loader",
+                    ],
+                },
+            ],
         },
-      ],
-    },
-    externals: {
-      jquery: "jQuery",
-      lodash: "lodash",
-      "@wordpress/api-fetch": ["wp", "apiFetch"],
-      "@wordpress/data": ["wp", "data"],
-      "@wordpress/dom-ready": ["wp", "domReady"],
-      "@wordpress/hooks": ["wp", "hooks"],
-      "@wordpress/keycodes": ["wp", "keycodes"],
-      "@wordpress/blocks": ["wp", "blocks"],
-      "@wordpress/i18n": ["wp", "i18n"],
-      "@wordpress/editor": ["wp", "editor"],
-      "@wordpress/components": ["wp", "components"],
-      "@wordpress/element": ["wp", "element"],
-      "@wordpress/blob": ["wp", "blob"],
-      "@wordpress/html-entities": ["wp", "htmlEntities"],
-      "@wordpress/compose": ["wp", "compose"],
-      "@wordpress/plugins": ["wp", "plugins"],
-      "@wordpress/edit-post": ["wp", "editPost"],
-      "@wordpress/block-editor": ["wp", "blockEditor"],
-      react: "React",
-      "react-dom": "ReactDOM",
-    },
-  };
-  return config;
+        externals: {
+            jquery: "jQuery",
+            lodash: "lodash",
+            "@wordpress/api-fetch": ["wp", "apiFetch"],
+            "@wordpress/data": ["wp", "data"],
+            "@wordpress/dom-ready": ["wp", "domReady"],
+            "@wordpress/hooks": ["wp", "hooks"],
+            "@wordpress/keycodes": ["wp", "keycodes"],
+            "@wordpress/blocks": ["wp", "blocks"],
+            "@wordpress/i18n": ["wp", "i18n"],
+            "@wordpress/editor": ["wp", "editor"],
+            "@wordpress/components": ["wp", "components"],
+            "@wordpress/element": ["wp", "element"],
+            "@wordpress/blob": ["wp", "blob"],
+            "@wordpress/html-entities": ["wp", "htmlEntities"],
+            "@wordpress/compose": ["wp", "compose"],
+            "@wordpress/plugins": ["wp", "plugins"],
+            "@wordpress/edit-post": ["wp", "editPost"],
+            "@wordpress/block-editor": ["wp", "blockEditor"],
+            react: "React",
+            "react-dom": "ReactDOM",
+        },
+    };
+    return config;
 };
