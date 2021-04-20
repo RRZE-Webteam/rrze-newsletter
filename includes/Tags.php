b893b227e026561fdf635d93e3cd6f665c1f1e92
rvdforst <rolf.v.d.forst@fau.de>
2021-04-20 17:56:15 +0200

    Update Tags.php

--- includes/Tags.php
+++ includes/Tags.php
@@ -7,34 +7,47 @@ defined('ABSPATH') || exit;
 class Tags
 {
     const SUPPORTED_TAGS = [
-        'FNAME'     => '', // First Name
-        'LNAME'     => '', // Last Name
-        'NAME'      => '', // Full Name
-        'EMAIL'     => '', // Email
-        'SUBSCURL'  => '', // Subscription Url
-        'USUBSCURL' => '', // Update Subscription Url
-        'SURL'      => '', // Website Url
+        // The subscriber's first name if it's available in the mailing list.
+        'FNAME'         => '',
+        // The subscriber's last name if it's available in the mailing list.
+        'LNAME'         => '',
+        // The subscriber's full name (generated from FNAME and LNAME).
+        'NAME'          => '',
+        // The subscriber's email address.
+        'EMAIL'         => '',
+        // The URL path + query to unsubscribe from the newsletters.
+        'UNSUB'         => '',
+        // The date the newsletter was sent.
+        'DATE'          => '',
+        // Displays the current year.
+        'CURRENT_YEAR'  => '',
     ];
 
-    public static function sanitizeTags(array $tags): array
+    public static function sanitizeTags(\WP_POST $post, array $tags = []): array
     {
         $tags = wp_parse_args($tags, self::SUPPORTED_TAGS);
         $tags = array_intersect_key($tags, self::SUPPORTED_TAGS);
-        return self::setDefaultValues($tags);
+        return self::setDefaultValues($post, $tags);
     }
 
-    protected static function setDefaultValues(array $tags): array
+    protected static function setDefaultValues(\WP_POST $post, array $tags): array
     {
         $options = (object) Settings::getOptions();
-        $subscPageSlug = $options->mailing_list_subsc_page_slug;
+
+        $name = !empty($tags['FNAME'] . $tags['LNAME']) ? trim(sprintf('%1$s %2$s', $tags['FNAME'], $tags['LNAME'])) : '';
+
         $email = Utils::sanitizeEmail($tags['EMAIL']);
         $encryptedEmail = Utils::encrypt($email);
-        $subscUrl = site_url($subscPageSlug);
-        $updateSubscUrl = site_url($subscPageSlug . '/?update=' . $encryptedEmail);
 
-        $tags['SUBSCURL'] = $subscUrl;
-        $tags['USUBSCURL'] = $email ? $updateSubscUrl : $subscUrl;
-        $tags['SLINK'] = site_url();
+        $subscPageSlug = $options->mailing_list_subsc_page_slug;
+        $unsubUrl = site_url($subscPageSlug . '/?update=' . $encryptedEmail);
+        $scheme = (string) parse_url($unsubUrl, PHP_URL_SCHEME);
+        $unsubPath = ltrim($unsubUrl, $scheme . '://');
+
+        $tags['NAME'] = $name;
+        $tags['UNSUB'] = $unsubPath;
+        $tags['DATE'] = get_the_time(get_option('date_format'), $post);
+        $tags['CURRENT_YEAR'] = date('Y');
 
         return $tags;
     }
