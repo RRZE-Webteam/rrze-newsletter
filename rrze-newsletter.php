b6559af70c2ccfe1e452a2588108db2a819ed91a
rvdforst <rolf.v.d.forst@fau.de>
2021-04-29 14:28:50 +0200

    Update rrze-newsletter.php

--- rrze-newsletter.php
+++ rrze-newsletter.php
@@ -4,7 +4,7 @@
 Plugin Name:      RRZE Newsletter
 Plugin URI:       https://github.com/RRZE-Webteam/rrze-newsletter
 Description:      Plugin for creating and sending HTML Newsletters.
-Version:          0.5.1
+Version:          0.5.2
 Author:           RRZE-Webteam
 Author URI:       https://blogs.fau.de/webworking/
 License:          GNU General Public License v2
