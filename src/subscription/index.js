7892fd58849372b12b9d1f3e6e841aa0702b5708
rvdforst <rolf.v.d.forst@fau.de>
2021-05-03 09:38:44 +0200

    Update index.js

--- src/subscription/index.js
+++ src/subscription/index.js
@@ -5,21 +5,23 @@ import "./style.scss";
 
 const jQuery = window && window.jQuery;
 
-(function ($) {
-    "use strict";
-
-    if ($("#unsubscribe-all").prop("checked")) {
-        $(".newsl-subsc-checkbox").prop("disabled", true);
-        $(".newsl-subsc-list").toggleClass("newsl-subsc-list--disabled");
+jQuery(document).ready(() => {
+    if (jQuery("#unsubscribe-all").prop("checked")) {
+        jQuery(".newsl-subsc-checkbox").prop("disabled", true);
+        jQuery(".newsl-subsc-list").toggleClass("newsl-subsc-list--disabled");
     }
-    $("#unsubscribe-all").change(function () {
-        $(".newsl-subsc-checkbox").prop("disabled", (i, v) => !v);
-        if ($(".newsl-subsc-list").hasClass("newsl-subsc-list--disabled")) {
-            $(".newsl-subsc-checkbox").prop("checked", true);
-            $(".newsl-subsc-list").removeClass("newsl-subsc-list--disabled");
+    jQuery("#unsubscribe-all").change(function () {
+        jQuery(".newsl-subsc-checkbox").prop("disabled", (i, v) => !v);
+        if (
+            jQuery(".newsl-subsc-list").hasClass("newsl-subsc-list--disabled")
+        ) {
+            jQuery(".newsl-subsc-checkbox").prop("checked", true);
+            jQuery(".newsl-subsc-list").removeClass(
+                "newsl-subsc-list--disabled"
+            );
         } else {
-            $(".newsl-subsc-checkbox").prop("checked", false);
-            $(".newsl-subsc-list").addClass("newsl-subsc-list--disabled");
+            jQuery(".newsl-subsc-checkbox").prop("checked", false);
+            jQuery(".newsl-subsc-list").addClass("newsl-subsc-list--disabled");
         }
     });
-})(jQuery);
+});
