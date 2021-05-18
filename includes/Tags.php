36e3296764cb8c8e9e446d2b49c452663b99c682
rvdforst <rolf.v.d.forst@fau.de>
2021-05-18 09:21:22 +0200

    Update Tags.php

--- includes/Tags.php
+++ includes/Tags.php
@@ -19,6 +19,8 @@ class Tags
         'EMAIL'         => '',
         // The URL to unsubscribe from the newsletters.
         'UNSUB'         => '',
+        // The URL to change the newsletter subscription.
+        'UPDATE'         => '',
         // The permalink of the newsletter.
         'PERMALINK'     => '',
         // The date the newsletter was sent.
@@ -48,7 +50,9 @@ class Tags
         $subscriptionPage = get_option('permalink_structure')
             ? get_page_link($subscriptionPageId)
             : add_query_arg('page_id', $subscriptionPageId, site_url());
-        $unsubUrl = add_query_arg('a', Utils::encryptQueryVar('update|' . $email), $subscriptionPage);
+
+        $unsubUrl = add_query_arg('a', Utils::encryptQueryVar('unsub|' . $email), $subscriptionPage);
+        $updateUrl = add_query_arg('a', Utils::encryptQueryVar('update|' . $email), $subscriptionPage);
 
         $archivePageBase = Archive::getPageBase();
         $archiveQuery = Utils::encryptQueryVar($postId . '|' . $email);
@@ -56,6 +60,7 @@ class Tags
 
         $tags['NAME'] = $name;
         $tags['UNSUB'] = $unsubUrl;
+        $tags['UPDATE'] = $updateUrl;
         $tags['PERMALINK'] = $archiveUrl;
         $tags['DATE'] = (string) get_the_time(get_option('date_format'), $postId);
         $tags['CURRENT_YEAR'] = date('Y');
