54a36838bcda205cd7f49761121238bbc7446a17
rvdforst <rolf.v.d.forst@fau.de>
2021-06-28 17:50:35 +0200

    Update Blocks.php

--- includes/Blocks.php
+++ includes/Blocks.php
@@ -28,51 +28,51 @@ final class Blocks
     {
         register_block_type(
             'rrze-newsletter/rss',
-            array(
+            [
                 'api_version' => 2,
-                'attributes'      => array(
-                    'feedURL' => array(
-                        'type'      => 'string',
-                        'default'   => '',
-                    ),
-                    'feedURL' => array(
-                        'type'      => 'string',
-                        'default'   => '',
-                    ),
-                    'itemsToShow' => array(
-                        'type'      => 'number',
-                        'default'   => 5,
-                    ),
-                    'displayExcerpt' => array(
-                        'type'      => 'boolean',
-                        'default'   => false,
-                    ),
-                    'displayAuthor' => array(
-                        'type'      => 'boolean',
-                        'default'   => false,
-                    ),
-                    'displayDate' => array(
-                        'type'      => 'boolean',
-                        'default'   => false,
-                    ),
-                    'excerptLength' => array(
-                        'type'      => 'number',
-                        'default'   => 25,
-                    ),
-                ),
-                'render_callback' => [__CLASS__, 'render_block_rss'],
-            )
+                'attributes' => [
+                    'feedURL' => [
+                        'type' => 'string',
+                        'default' => '',
+                    ],
+                    'feedURL' => [
+                        'type' => 'string',
+                        'default' => '',
+                    ],
+                    'itemsToShow' => [
+                        'type' => 'number',
+                        'default' => 5,
+                    ],
+                    'displayExcerpt' => [
+                        'type' => 'boolean',
+                        'default' => false,
+                    ],
+                    'displayAuthor' => [
+                        'type' => 'boolean',
+                        'default' => false,
+                    ],
+                    'displayDate' => [
+                        'type' => 'boolean',
+                        'default' => false,
+                    ],
+                    'excerptLength' => [
+                        'type' => 'number',
+                        'default' => 25,
+                    ]
+                ],
+                'render_callback' => [__CLASS__, 'renderBlockRSS'],
+            ]
         );
     }
 
     /**
      * Renders the `rrze-newsletter/rss` block on server.
-     * @param array $attributes The block attributes.
+     * @param array $atts The block attributes.
      * @return string Returns the block content with received rss items.
      */
-    public static function render_block_rss($attributes)
+    public static function renderBlockRSS($atts)
     {
-        $rss = fetch_feed($attributes['feedURL']);
+        $rss = fetch_feed($atts['feedURL']);
 
         if (is_wp_error($rss)) {
             return '<div class="components-placeholder"><div class="notice notice-error"><strong>' . __('RSS Error:', 'rrze-newsletter') . '</strong> ' . $rss->get_error_message() . '</div></div>';
@@ -82,27 +82,27 @@ final class Blocks
             return '<div class="components-placeholder"><div class="notice notice-error">' . __('An error has occurred, which probably means the feed is down. Try again later.', 'rrze-newsletter') . '</div></div>';
         }
 
-        $rss_items  = $rss->get_items(0, $attributes['itemsToShow']);
-        $list_items = '';
-        foreach ($rss_items as $item) {
+        $rssItems  = $rss->get_items(0, $atts['itemsToShow']);
+        $listItems = '';
+        foreach ($rssItems as $item) {
             $title = esc_html(trim(strip_tags($item->get_title())));
             if (empty($title)) {
-                $title = __('(no title)');
+                $title = __('(no title)', 'rrze-newsletter');
             }
             $link = $item->get_link();
             $link = esc_url($link);
             if ($link) {
                 $title = "<a href='{$link}'>{$title}</a>";
             }
-            $title = "<div class='wp-block-rss__item-title'>{$title}</div>";
+            $title = "<h3>{$title}</h3>";
 
             $date = '';
-            if ($attributes['displayDate']) {
+            if ($atts['displayDate']) {
                 $date = $item->get_date('U');
 
                 if ($date) {
                     $date = sprintf(
-                        '<time datetime="%1$s" class="wp-block-rss__item-publish-date">%2$s</time> ',
+                        '<time datetime="%1$s">%2$s</time> ',
                         date_i18n(get_option('c'), $date),
                         date_i18n(get_option('date_format'), $date)
                     );
@@ -110,22 +110,22 @@ final class Blocks
             }
 
             $author = '';
-            if ($attributes['displayAuthor']) {
+            if ($atts['displayAuthor']) {
                 $author = $item->get_author();
                 if (is_object($author)) {
                     $author = $author->get_name();
-                    $author = '<span class="wp-block-rss__item-author">' . sprintf(
+                    $author = '<span>' . sprintf(
                         /* translators: %s: the author. */
-                        __('by %s'),
+                        __('by %s', 'rrze-newsletter'),
                         esc_html(strip_tags($author))
                     ) . '</span>';
                 }
             }
 
             $excerpt = '';
-            if ($attributes['displayExcerpt']) {
+            if ($atts['displayExcerpt']) {
                 $excerpt = html_entity_decode($item->get_description(), ENT_QUOTES, get_option('blog_charset'));
-                $excerpt = esc_attr(wp_trim_words($excerpt, $attributes['excerptLength'], ' [&hellip;]'));
+                $excerpt = esc_attr(wp_trim_words($excerpt, $atts['excerptLength'], ' [&hellip;]'));
 
                 // Change existing [...] to [&hellip;].
                 if ('[...]' === substr($excerpt, -5)) {
@@ -135,12 +135,88 @@ final class Blocks
                 $excerpt = '<div class="wp-block-rss__item-excerpt">' . esc_html($excerpt) . '</div>';
             }
 
-            $list_items .= "<li class='wp-block-rss__item'>{$title}{$date}{$author}{$excerpt}</li>";
+            $listItems .= "{$title}{$date}{$author}{$excerpt}";
         }
 
         $classnames = [];
-        $wrapper_attributes = get_block_wrapper_attributes(['class' => implode(' ', $classnames)]);
+        $wrapperAtts = get_block_wrapper_attributes(['class' => implode(' ', $classnames)]);
 
-        return sprintf('<ul %s>%s</ul>', $wrapper_attributes, $list_items);
+        return sprintf('<div %s>%s</div>', $wrapperAtts, $listItems);
+    }
+
+    /**
+     * Renders the `rrze-newsletter/rss` block in a MJML format.
+     * @param array $atts The block attributes.
+     * @return string Returns the block content with received rss items.
+     */
+    public static function mjmlRenderBlockRSS($atts)
+    {
+        $rss = fetch_feed($atts['feedURL']);
+
+        if (is_wp_error($rss)) {
+            return '';
+        }
+
+        if (!$rss->get_item_quantity()) {
+            return '';
+        }
+
+        $rssItems  = $rss->get_items(0, $atts['itemsToShow']);
+        $listItems = '';
+        foreach ($rssItems as $item) {
+            $title = esc_html(trim(strip_tags($item->get_title())));
+            if (empty($title)) {
+                $title = __('(no title)', 'rrze-newsletter');
+            }
+            $link = $item->get_link();
+            $link = esc_url($link);
+            if ($link) {
+                $title = "<a href='{$link}'>{$title}</a>";
+            }
+            $title = "<h3>{$title}</h3>";
+
+            $date = '';
+            if ($atts['displayDate']) {
+                $date = $item->get_date('U');
+
+                if ($date) {
+                    $date = sprintf(
+                        '<span>%2$s</span> ',
+                        date_i18n(get_option('c'), $date),
+                        date_i18n(get_option('date_format'), $date)
+                    );
+                }
+            }
+
+            $author = '';
+            if ($atts['displayAuthor']) {
+                $author = $item->get_author();
+                if (is_object($author)) {
+                    $author = $author->get_name();
+                    $author = '<span class="wp-block-rss__item-author">' . sprintf(
+                        /* translators: %s: the author. */
+                        __('by %s', 'rrze-newsletter'),
+                        esc_html(strip_tags($author))
+                    ) . '</span>';
+                }
+            }
+
+            $excerpt = '';
+            if ($atts['displayExcerpt']) {
+                $excerpt = html_entity_decode($item->get_description(), ENT_QUOTES, get_option('blog_charset'));
+                $excerpt = esc_attr(wp_trim_words($excerpt, $atts['excerptLength'], ' [&hellip;]'));
+
+                // Change existing [...] to [&hellip;].
+                if ('[...]' === substr($excerpt, -5)) {
+                    $excerpt = substr($excerpt, 0, -5) . '[&hellip;]';
+                }
+
+                $excerpt = '<p>' . esc_html($excerpt) . '</p>';
+            }
+
+            $listItems .= "{$title}{$date}{$author}{$excerpt}";
+        }
+
+        return $listItems;
     }
 }
