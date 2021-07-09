aeb655382574ad8a3445dc77f712beeb15d83a0f
rvdforst <rolf.v.d.forst@fau.de>
2021-07-09 15:03:02 +0200

    Update Editor.php

--- includes/Editor.php
+++ includes/Editor.php
@@ -73,7 +73,8 @@ final class Editor
             'core/social-link',
             'core/social-links',
             'rrze-newsletter/post-inserter',
-            'rrze-newsletter/rss'
+            'rrze-newsletter/rss',
+            'rrze-newsletter/ics'
         ];
     }
 
