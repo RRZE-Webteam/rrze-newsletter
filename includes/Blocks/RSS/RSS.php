1472311650f1b60378621967456ca105d2bcb984
rvdforst <rolf.v.d.forst@fau.de>
2021-08-18 12:10:51 +0200

    Update RSS.php

--- includes/Blocks/RSS/RSS.php
+++ includes/Blocks/RSS/RSS.php
@@ -151,7 +151,8 @@ class RSS
             $textStyle .= $atts['textColor'] ? 'color:' . $atts['textColor'] : '';
             $textStyle = $textStyle ? ' style="' . $textStyle . '"' : '';
             $feedItems = sprintf('<p%1$s>%2$s</p>', $textStyle, __('There are no items available.', 'rrze-newsletter'));
-            update_post_meta($atts['postId'], 'rrze_newsletter_rss_block_empty', 1);
+        } else {
+            wp_cache_set('rrze_newsletter_rss_block_not_empty', 1, $atts['postId']);
         }
 
         return $feedItems;
