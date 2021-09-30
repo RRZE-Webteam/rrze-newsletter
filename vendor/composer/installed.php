2923c3900c14fc9961b6bd2179edd9425f95c152
rvdforst <rolf.v.d.forst@fau.de>
2021-09-30 11:11:35 +0200

    Add vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.7.0',
+        'version' => '1.7.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'rrze/rrze-newsletter',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'johngrogg/ics-parser' => array(
+            'pretty_version' => 'v2.2.2',
+            'version' => '2.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../johngrogg/ics-parser',
+            'aliases' => array(),
+            'reference' => '69c80471a0a99142ebc72b21c2bc084e81a7c4f4',
+            'dev_requirement' => false,
+        ),
+        'rlanvin/php-rrule' => array(
+            'pretty_version' => 'v2.2.2',
+            'version' => '2.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rlanvin/php-rrule',
+            'aliases' => array(),
+            'reference' => '4e979ff9b1122e7b5f4b4faf87a000396af12d16',
+            'dev_requirement' => false,
+        ),
+        'rrze/rrze-newsletter' => array(
+            'pretty_version' => '1.7.0',
+            'version' => '1.7.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
