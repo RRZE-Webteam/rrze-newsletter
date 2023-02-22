57f8a86cd6202761f6468e23f36ed395cdb1b141
rvdforst <rolf.v.d.forst@fau.de>
2023-02-22 17:10:41 +0100

    Update index.js

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,10 +1,15 @@
 /**
  * WordPress dependencies
  */
-import { unregisterBlockStyle } from "@wordpress/blocks";
+import {
+    registerBlockStyle,
+    unregisterBlockStyle,
+    unregisterBlockVariation,
+} from "@wordpress/blocks";
 import domReady from "@wordpress/dom-ready";
-import { addFilter } from "@wordpress/hooks";
+import { addFilter, removeFilter } from "@wordpress/hooks";
 import { registerPlugin } from "@wordpress/plugins";
+import { __ } from "@wordpress/i18n";
 
 /**
  * Plugin dependencies
@@ -27,7 +32,14 @@ domReady(() => {
     unregisterBlockStyle("core/separator", "dots");
     unregisterBlockStyle("core/social-links", "logos-only");
     unregisterBlockStyle("core/social-links", "pill-shape");
+    /* Unregister "row" group block variation */
+    unregisterBlockVariation("core/group", "group-row");
 });
+
+/* Remove Duotone filters */
+removeFilter("blocks.registerBlockType", "core/editor/duotone/add-attributes");
+removeFilter("editor.BlockEdit", "core/editor/duotone/with-editor-controls");
+removeFilter("editor.BlockListBlock", "core/editor/duotone/with-styles");
 
 addFilter(
     "blocks.registerBlockType",
@@ -37,7 +49,8 @@ addFilter(
         if (
             "core/paragraph" === name ||
             "core/buttons" === name ||
-            "core/columns" === name
+            "core/columns" === name ||
+            "core/separator" === name
         ) {
             settings.supports = { ...settings.supports, align: [] };
         }
@@ -48,7 +61,27 @@ addFilter(
     }
 );
 
+registerBlockStyle("core/social-links", {
+    name: "circle-black",
+    label: __("Circle Black", "rrze-newsletter"),
+});
+
+registerBlockStyle("core/social-links", {
+    name: "circle-white",
+    label: __("Circle White", "rrze-newsletter"),
+});
+
+registerBlockStyle("core/social-links", {
+    name: "filled-black",
+    label: __("Black", "rrze-newsletter"),
+});
+
+registerBlockStyle("core/social-links", {
+    name: "filled-white",
+    label: __("White", "rrze-newsletter"),
+});
+
 registerPlugin("rrze-newsletter-plugin", {
     render: NestedColumnsDetection,
-    icon: null
+    icon: null,
 });
