be169d20081e4a0c862c61673aa7f2fe9aadd7e2
rvdforst <rolf.v.d.forst@fau.de>
2023-04-19 16:12:27 +0200

    Update vendor files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,51 +1,50 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-main',
-    'version' => 'dev-main',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'rrze/rrze-newsletter',
+        'pretty_version' => '1.10.6',
+        'version' => '1.10.6.0',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '07073411074db0fc4c49b9701338ab34675fe8c2',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '07073411074db0fc4c49b9701338ab34675fe8c2',
+    'versions' => array(
+        'html2text/html2text' => array(
+            'pretty_version' => '4.3.1',
+            'version' => '4.3.1.0',
+            'reference' => '61ad68e934066a6f8df29a3d23a6460536d0855c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../html2text/html2text',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'johngrogg/ics-parser' => array(
+            'pretty_version' => 'v2.2.2',
+            'version' => '2.2.2.0',
+            'reference' => '69c80471a0a99142ebc72b21c2bc084e81a7c4f4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../johngrogg/ics-parser',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rlanvin/php-rrule' => array(
+            'pretty_version' => 'v2.4.0',
+            'version' => '2.4.0.0',
+            'reference' => '2acd9950e803ea65514d6440212d39096df9c528',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rlanvin/php-rrule',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rrze/rrze-newsletter' => array(
+            'pretty_version' => '1.10.6',
+            'version' => '1.10.6.0',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'html2text/html2text' => 
-    array (
-      'pretty_version' => '4.3.1',
-      'version' => '4.3.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '61ad68e934066a6f8df29a3d23a6460536d0855c',
-    ),
-    'johngrogg/ics-parser' => 
-    array (
-      'pretty_version' => 'v2.2.2',
-      'version' => '2.2.2.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '69c80471a0a99142ebc72b21c2bc084e81a7c4f4',
-    ),
-    'rlanvin/php-rrule' => 
-    array (
-      'pretty_version' => 'v2.4.0',
-      'version' => '2.4.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '2acd9950e803ea65514d6440212d39096df9c528',
-    ),
-  ),
 );
