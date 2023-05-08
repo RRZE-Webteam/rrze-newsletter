a094afe2f6020d6cf3b7098d6728d7c0aad021e1
rvdforst <rolf.v.d.forst@fau.de>
2023-05-08 18:05:45 +0200

    Create script.js

---
+++ src/subscription/script.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function ($) {
+    "use strict";
+    if ($(".rrze-newsletter-subscription #unsubscribe-all").prop("checked")) {
+        $(".rrze-newsletter-subscription .checkbox").prop("disabled", true);
+        $(".rrze-newsletter-subscription .list").toggleClass("list--disabled");
+    }
+    $(document).on(
+        "change",
+        ".rrze-newsletter-subscription #unsubscribe-all",
+        () => {
+            $(".rrze-newsletter-subscription .checkbox").prop(
+                "disabled",
+                (i, v) => !v
+            );
+            if (
+                $(".rrze-newsletter-subscription .list").hasClass(
+                    "list--disabled"
+                )
+            ) {
+                $(".rrze-newsletter-subscription .checkbox").prop(
+                    "checked",
+                    true
+                );
+                $(".rrze-newsletter-subscription .list").removeClass(
+                    "list--disabled"
+                );
+            } else {
+                $(".rrze-newsletter-subscription .checkbox").prop(
+                    "checked",
+                    false
+                );
+                $(".rrze-newsletter-subscription .list").addClass(
+                    "list--disabled"
+                );
+            }
+        }
+    );
+});
