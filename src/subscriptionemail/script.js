de23ce9c1a61658109b2652c68e253ba6e7dd426
rvdforst <rolf.v.d.forst@fau.de>
2023-05-08 18:05:50 +0200

    Create script.js

---
+++ src/subscriptionemail/script.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import mjml2html from "mjml-browser";
+
+jQuery(document).ready(function ($) {
+    "use strict";
+    $.ajax({
+        type: "post",
+        url: subscriptionEmail.ajaxUrl,
+        data: {
+            action: "mjml2html",
+            from: subscriptionEmail.from,
+            fromName: subscriptionEmail.fromName,
+            replyTo: subscriptionEmail.replyTo,
+            to: subscriptionEmail.to,
+            subject: subscriptionEmail.subject,
+            body: mjml2html(subscriptionEmail.mjml, {
+                keepComments: false
+            }),
+        },
+    });
+});
