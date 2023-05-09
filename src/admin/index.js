58e6b9ae4de90b00c2298b2fef5586bec8faa2cf
rvdforst <rolf.v.d.forst@fau.de>
2023-05-09 17:56:35 +0200

    Update index.js

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,20 +1,4 @@
 /**
  * Plugin dependencies
  */
-import $ from "jquery";
-
-$(document).ready(() => {
-    $(document).on(
-        "click",
-        ".rrze-newsletter-activation-notice .notice-dismiss",
-        () => {
-            const data = {
-                action: "rrze_newsletter_activation_notice_dismiss"
-            };
-            const { ajaxurl } =
-                window &&
-                window.rrze_newsletter_activation_notice_dismiss_params;
-            $.post(ajaxurl, data, () => null);
-        }
-    );
-});
+import "./script.js";
