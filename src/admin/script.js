b965ca65c6f5cdedea765732f0844e39f66a38df
rvdforst <rolf.v.d.forst@fau.de>
2023-05-09 17:56:38 +0200

    Create script.js

---
+++ src/admin/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(($) => {
+    $(document).on(
+        "click",
+        ".rrze-newsletter-activation-notice .notice-dismiss",
+        () => {
+            const data = {
+                action: "rrze_newsletter_activation_notice_dismiss",
+            };
+            const { ajaxurl } =
+                window &&
+                window.rrze_newsletter_activation_notice_dismiss_params;
+            $.post(ajaxurl, data, () => null);
+        }
+    );
+});
