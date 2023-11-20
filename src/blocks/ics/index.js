5129dae93eeccc5c074726a5fbf90110345e3cac
rvdforst <rolf.v.d.forst@fau.de>
2023-11-20 16:57:08 +0100

    Update index.js

--- src/blocks/ics/index.js
+++ src/blocks/ics/index.js
@@ -8,12 +8,14 @@ import { __ } from "@wordpress/i18n";
  * Plugin dependencies
  */
 import "./style.scss";
+import blockDefinition from "./block.json";
 import { ICS_BLOCK_NAME } from "./consts";
 import Icon from "./icon";
 import ICSEdit from "./edit";
 
 export default () => {
     registerBlockType(ICS_BLOCK_NAME, {
+        ...blockDefinition,
         title: __("ICS", "rrze-newsletter"),
         category: "widgets",
         icon: Icon,
@@ -23,51 +25,5 @@ export default () => {
         ),
         keywords: ["ics", "ical", "feed"],
         edit: ICSEdit,
-        attributes: {
-            feedURL: {
-                type: "string",
-                default: ""
-            },
-            itemsToShow: {
-                type: "number",
-                default: 5
-            },
-            displayDescription: {
-                type: "boolean",
-                default: false
-            },
-            displayLocation: {
-                type: "boolean",
-                default: false
-            },
-            displayOrganizer: {
-                type: "boolean",
-                default: false
-            },
-            descriptionLimit: {
-                type: "boolean",
-                default: false
-            },
-            descriptionLength: {
-                type: "number",
-                default: 25
-            },
-            textFontSize: {
-                type: "number",
-                default: 16
-            },
-            headingFontSize: {
-                type: "number",
-                default: 25
-            },
-            textColor: {
-                type: "string",
-                default: "#000"
-            },
-            headingColor: {
-                type: "string",
-                default: "#000"
-            }
-        }
     });
 };
