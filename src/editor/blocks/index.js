743f6565f985b550e401afe9014b0f4aef4f4418
rvdforst <rolf.v.d.forst@fau.de>
2023-11-22 12:56:44 +0100

    Create index.js

---
+++ src/editor/blocks/index.js
@@ -0,0 +1,10 @@
+/**
+ * WordPress dependencies
+ */
+import registerPostInserterBlock from './post-inserter';
+import registerICSBlock from './ics';
+import registerRSSBlock from './rss';
+
+registerPostInserterBlock();
+registerICSBlock();
+registerRSSBlock();
