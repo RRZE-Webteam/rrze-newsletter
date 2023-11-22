0e370069f8afb6e1b62ea51b5daa22b8cbcc778f
rvdforst <rolf.v.d.forst@fau.de>
2023-11-22 12:56:39 +0100

    Create index.js

---
+++ src/editor/blocks/ics/index.js
@@ -0,0 +1,29 @@
+/**
+ * WordPress dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Plugin dependencies
+ */
+import './style.scss';
+import blockDefinition from './block.json';
+import { ICS_BLOCK_NAME } from './consts';
+import Icon from './icon';
+import ICSEdit from './edit';
+
+export default () => {
+	registerBlockType( ICS_BLOCK_NAME, {
+		...blockDefinition,
+		title: __( 'ICS', 'rrze-newsletter' ),
+		category: 'widgets',
+		icon: Icon,
+		description: __(
+			'Display entries from any ICS or ICal feed.',
+			'rrze-newsletter'
+		),
+		keywords: [ 'ics', 'ical', 'feed' ],
+		edit: ICSEdit,
+	} );
+};
