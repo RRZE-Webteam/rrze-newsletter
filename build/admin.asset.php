691d47c7141aadd3facfed65ff2c58bc0af930d8
rvdforst <rolf.v.d.forst@fau.de>
2023-11-22 12:55:02 +0100

    Update build files

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '63d7a16429a608b83b94');
+<?php return array('dependencies' => array('jquery'), 'version' => '161fbd5ff80f28e15422');
