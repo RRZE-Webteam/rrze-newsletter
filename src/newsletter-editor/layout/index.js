d0ff001708ef2e3d1ecbac202fc16f3623da59c8
Rolf <rolf.v.d.forst@fau.de>
2024-08-07 16:27:51 +0200

    Update index.js

--- src/newsletter-editor/layout/index.js
+++ src/newsletter-editor/layout/index.js
@@ -1,286 +1,285 @@
 /**
  * External dependencies
  */
-import { isEqual, find } from 'lodash';
+import { isEqual, find } from "lodash";
 
 /**
  * WordPress dependencies
  */
-import { compose } from '@wordpress/compose';
-import { parse, serialize } from '@wordpress/blocks';
-import { __ } from '@wordpress/i18n';
-import { withDispatch, withSelect } from '@wordpress/data';
-import { Fragment, useState, useEffect, useMemo } from '@wordpress/element';
-import { Button, Modal, TextControl, Spinner } from '@wordpress/components';
+import { compose } from "@wordpress/compose";
+import { parse, serialize } from "@wordpress/blocks";
+import { __ } from "@wordpress/i18n";
+import { withDispatch, withSelect } from "@wordpress/data";
+import { Fragment, useState, useEffect, useMemo } from "@wordpress/element";
+import { Button, Modal, TextControl, Spinner } from "@wordpress/components";
 
 /**
  * Plugin dependencies
  */
-import { useLayoutsState } from '../../utils/hooks';
-import { LAYOUT_CPT_SLUG, NEWSLETTER_CPT_SLUG } from '../../utils/consts';
-import { isUserDefinedLayout } from '../../utils';
-import './style.scss';
-import NewsletterPreview from '../../components/newsletter-preview';
+import { useLayoutsState } from "../../utils/hooks";
+import { LAYOUT_CPT_SLUG, NEWSLETTER_CPT_SLUG } from "../../utils/consts";
+import {
+    isUserDefinedLayout,
+    getBaseUrl,
+    convertRelativeUrlsToAbsolute,
+} from "../../utils";
+import "./style.scss";
+import NewsletterPreview from "../../components/newsletter-preview";
 
-export default compose( [
-	withSelect( ( select ) => {
-		const { getEditedPostAttribute, isEditedPostEmpty, getCurrentPostId } =
-			select( 'core/editor' );
-		const { getBlocks } = select( 'core/block-editor' );
-		const meta = getEditedPostAttribute( 'meta' );
-		const {
-			rrze_newsletter_template_id: layoutId,
-			rrze_newsletter_background_color,
-			rrze_newsletter_font_body,
-			rrze_newsletter_font_header,
-		} = meta;
-		return {
-			layoutId,
-			postTitle: getEditedPostAttribute( 'title' ),
-			postBlocks: getBlocks(),
-			isEditedPostEmpty: isEditedPostEmpty(),
-			currentPostId: getCurrentPostId(),
-			stylingMeta: {
-				rrze_newsletter_background_color,
-				rrze_newsletter_font_body,
-				rrze_newsletter_font_header,
-			},
-		};
-	} ),
-	withDispatch( ( dispatch, { currentPostId, stylingMeta } ) => {
-		const { replaceBlocks } = dispatch( 'core/block-editor' );
-		const { editPost } = dispatch( 'core/editor' );
-		const { saveEntityRecord } = dispatch( 'core' );
-		return {
-			replaceBlocks,
-			saveLayoutIdMeta: ( id ) => {
-				editPost( { meta: { rrze_newsletter_template_id: id } } );
-				saveEntityRecord( 'postType', NEWSLETTER_CPT_SLUG, {
-					id: currentPostId,
-					meta: { rrze_newsletter_template_id: id, ...stylingMeta },
-				} );
-			},
-			saveLayout: ( payload ) =>
-				saveEntityRecord( 'postType', LAYOUT_CPT_SLUG, {
-					status: 'publish',
-					...payload,
-				} ),
-		};
-	} ),
-] )( ( {
-	saveLayoutIdMeta,
-	layoutId,
-	replaceBlocks,
-	saveLayout,
-	postBlocks,
-	postTitle,
-	isEditedPostEmpty,
-	stylingMeta,
-} ) => {
-	const [ warningModalVisible, setWarningModalVisible ] = useState( false );
-	const { layouts, isFetchingLayouts } = useLayoutsState();
+export default compose([
+    withSelect((select) => {
+        const { getEditedPostAttribute, isEditedPostEmpty, getCurrentPostId } =
+            select("core/editor");
+        const { getBlocks } = select("core/block-editor");
+        const meta = getEditedPostAttribute("meta");
+        const {
+            rrze_newsletter_template_id: layoutId,
+            rrze_newsletter_background_color,
+            rrze_newsletter_font_body,
+            rrze_newsletter_font_header,
+        } = meta;
+        return {
+            layoutId,
+            postTitle: getEditedPostAttribute("title"),
+            postBlocks: getBlocks(),
+            isEditedPostEmpty: isEditedPostEmpty(),
+            currentPostId: getCurrentPostId(),
+            stylingMeta: {
+                rrze_newsletter_background_color,
+                rrze_newsletter_font_body,
+                rrze_newsletter_font_header,
+            },
+        };
+    }),
+    withDispatch((dispatch, { currentPostId, stylingMeta }) => {
+        const { replaceBlocks } = dispatch("core/block-editor");
+        const { editPost } = dispatch("core/editor");
+        const { saveEntityRecord } = dispatch("core");
+        return {
+            replaceBlocks,
+            saveLayoutIdMeta: (id) => {
+                editPost({ meta: { rrze_newsletter_template_id: id } });
+                saveEntityRecord("postType", NEWSLETTER_CPT_SLUG, {
+                    id: currentPostId,
+                    meta: { rrze_newsletter_template_id: id, ...stylingMeta },
+                });
+            },
+            saveLayout: (payload) =>
+                saveEntityRecord("postType", LAYOUT_CPT_SLUG, {
+                    status: "publish",
+                    ...payload,
+                }),
+        };
+    }),
+])(({
+    saveLayoutIdMeta,
+    layoutId,
+    replaceBlocks,
+    saveLayout,
+    postBlocks,
+    postTitle,
+    isEditedPostEmpty,
+    stylingMeta,
+}) => {
+    const [warningModalVisible, setWarningModalVisible] = useState(false);
+    const { layouts, isFetchingLayouts } = useLayoutsState();
 
-	const [ usedLayout, setUsedLayout ] = useState( {} );
+    const [usedLayout, setUsedLayout] = useState({});
 
-	useEffect( () => {
-		setUsedLayout( find( layouts, { ID: layoutId } ) || {} );
-	}, [ layouts.length ] );
+    useEffect(() => {
+        setUsedLayout(find(layouts, { ID: layoutId }) || {});
+    }, [layouts.length]);
 
-	const blockPreview = useMemo( () => {
-		return usedLayout.post_content
-			? parse( usedLayout.post_content )
-			: null;
-	}, [ usedLayout ] );
+    const absolutePostContent = convertRelativeUrlsToAbsolute(
+        usedLayout.post_content,
+        getBaseUrl()
+    );
 
-	const clearPost = () => {
-		const clientIds = postBlocks.map( ( { clientId } ) => clientId );
-		if ( clientIds && clientIds.length ) {
-			replaceBlocks( clientIds, [] );
-		}
-	};
+    const blockPreview = useMemo(() => {
+        return usedLayout.post_content ? parse(absolutePostContent) : null;
+    }, [usedLayout]);
 
-	const [ isSavingLayout, setIsSavingLayout ] = useState( false );
-	const [ isManageModalVisible, setIsManageModalVisible ] = useState( null );
-	const [ newLayoutName, setNewLayoutName ] = useState( postTitle );
+    const clearPost = () => {
+        const clientIds = postBlocks.map(({ clientId }) => clientId);
+        if (clientIds && clientIds.length) {
+            replaceBlocks(clientIds, []);
+        }
+    };
 
-	const handleLayoutUpdate = ( updatedLayout ) => {
-		setIsSavingLayout( false );
-		// Set this new layout as the newsletter's layout
-		saveLayoutIdMeta( updatedLayout.id );
+    const [isSavingLayout, setIsSavingLayout] = useState(false);
+    const [isManageModalVisible, setIsManageModalVisible] = useState(null);
+    const [newLayoutName, setNewLayoutName] = useState(postTitle);
 
-		// Update the layout preview
-		// The shape of this data is different than the API response for CPT
-		setUsedLayout( {
-			...updatedLayout,
-			post_content: updatedLayout.content.raw,
-			post_title: updatedLayout.title.raw,
-			post_type: LAYOUT_CPT_SLUG,
-		} );
-	};
+    const handleLayoutUpdate = (updatedLayout) => {
+        setIsSavingLayout(false);
+        // Set this new layout as the newsletter's layout
+        saveLayoutIdMeta(updatedLayout.id);
 
-	const postContent = useMemo(
-		() => serialize( postBlocks ),
-		[ postBlocks ]
-	);
-	const isPostContentSameAsLayout =
-		postContent === usedLayout.post_content &&
-		isEqual( usedLayout.meta, stylingMeta );
+        // Update the layout preview
+        // The shape of this data is different than the API response for CPT
+        setUsedLayout({
+            ...updatedLayout,
+            post_content: updatedLayout.content.raw,
+            post_title: updatedLayout.title.raw,
+            post_type: LAYOUT_CPT_SLUG,
+        });
+    };
 
-	const handleSaveAsLayout = () => {
-		setIsSavingLayout( true );
-		const updatePayload = {
-			title: newLayoutName,
-			content: postContent,
-			meta: stylingMeta,
-		};
-		saveLayout( updatePayload ).then( ( newLayout ) => {
-			setIsManageModalVisible( false );
+    const postContent = useMemo(() => serialize(postBlocks), [postBlocks]);
+    const isPostContentSameAsLayout =
+        postContent === usedLayout.post_content &&
+        isEqual(usedLayout.meta, stylingMeta);
 
-			handleLayoutUpdate( newLayout );
-		} );
-	};
+    const handleSaveAsLayout = () => {
+        setIsSavingLayout(true);
+        const updatePayload = {
+            title: newLayoutName,
+            content: postContent,
+            meta: stylingMeta,
+        };
+        saveLayout(updatePayload).then((newLayout) => {
+            setIsManageModalVisible(false);
 
-	const handeLayoutUpdate = () => {
-		if (
-			confirm(
-				__(
-					'Are you sure you want to overwrite this layout?',
-					'rrze-newsletter'
-				)
-			)
-		) {
-			setIsSavingLayout( true );
-			const updatePayload = {
-				id: usedLayout.ID,
-				content: postContent,
-				meta: stylingMeta,
-			};
-			saveLayout( updatePayload ).then( handleLayoutUpdate );
-		}
-	};
+            handleLayoutUpdate(newLayout);
+        });
+    };
 
-	const isUsingCustomLayout = isUserDefinedLayout( usedLayout );
+    const handeLayoutUpdate = () => {
+        if (
+            confirm(
+                __(
+                    "Are you sure you want to overwrite this layout?",
+                    "rrze-newsletter"
+                )
+            )
+        ) {
+            setIsSavingLayout(true);
+            const updatePayload = {
+                id: usedLayout.ID,
+                content: postContent,
+                meta: stylingMeta,
+            };
+            saveLayout(updatePayload).then(handleLayoutUpdate);
+        }
+    };
 
-	return (
-		<Fragment>
-			{ Boolean( layoutId && isFetchingLayouts ) && (
-				<div className="rrze-newsletter-layouts__spinner">
-					<Spinner />
-				</div>
-			) }
-			{ blockPreview !== null && (
-				<div className="rrze-newsletter-layouts">
-					<div className="rrze-newsletter-layouts__item">
-						<div className="rrze-newsletter-layouts__item-preview">
-							<NewsletterPreview
-								meta={ usedLayout.meta }
-								blocks={ blockPreview }
-								viewportWidth={ 600 }
-							/>
-						</div>
-						<div className="rrze-newsletter-layouts__item-label">
-							{ usedLayout.post_title }
-						</div>
-					</div>
-				</div>
-			) }
-			<div className="rrze-newsletter-buttons-group rrze-newsletter-buttons-group--spaced">
-				<Button
-					isPrimary
-					disabled={ isEditedPostEmpty || isSavingLayout }
-					onClick={ () => setIsManageModalVisible( true ) }
-				>
-					{ __( 'Save new layout', 'rrze-newsletter' ) }
-				</Button>
+    const isUsingCustomLayout = isUserDefinedLayout(usedLayout);
 
-				{ isUsingCustomLayout && (
-					<Button
-						isSecondary
-						disabled={ isPostContentSameAsLayout || isSavingLayout }
-						onClick={ handeLayoutUpdate }
-					>
-						{ __( 'Update layout', 'rrze-newsletter' ) }
-					</Button>
-				) }
-			</div>
+    return (
+        <Fragment>
+            {Boolean(layoutId && isFetchingLayouts) && (
+                <div className="rrze-newsletter-layouts__spinner">
+                    <Spinner />
+                </div>
+            )}
+            {blockPreview !== null && (
+                <div className="rrze-newsletter-layouts">
+                    <div className="rrze-newsletter-layouts__item">
+                        <div className="rrze-newsletter-layouts__item-preview">
+                            <NewsletterPreview
+                                meta={usedLayout.meta}
+                                blocks={blockPreview}
+                                viewportWidth={600}
+                            />
+                        </div>
+                        <div className="rrze-newsletter-layouts__item-label">
+                            {usedLayout.post_title}
+                        </div>
+                    </div>
+                </div>
+            )}
+            <div className="rrze-newsletter-buttons-group rrze-newsletter-buttons-group--spaced">
+                <Button
+                    isPrimary
+                    disabled={isEditedPostEmpty || isSavingLayout}
+                    onClick={() => setIsManageModalVisible(true)}
+                >
+                    {__("Save new layout", "rrze-newsletter")}
+                </Button>
 
-			<br />
+                {isUsingCustomLayout && (
+                    <Button
+                        isSecondary
+                        disabled={isPostContentSameAsLayout || isSavingLayout}
+                        onClick={handeLayoutUpdate}
+                    >
+                        {__("Update layout", "rrze-newsletter")}
+                    </Button>
+                )}
+            </div>
 
-			<Button
-				isSecondary
-				isLink
-				isDestructive
-				onClick={ () => setWarningModalVisible( true ) }
-			>
-				{ __( 'Reset newsletter layout', 'rrze-newsletter' ) }
-			</Button>
+            <br />
 
-			{ isManageModalVisible && (
-				<Modal
-					className="rrze-newsletter__modal"
-					title={ __(
-						'Save newsletter as a layout',
-						'rrze-newsletter'
-					) }
-					onRequestClose={ () => setIsManageModalVisible( null ) }
-				>
-					<TextControl
-						label={ __( 'Title', 'rrze-newsletter' ) }
-						disabled={ isSavingLayout }
-						value={ newLayoutName }
-						onChange={ setNewLayoutName }
-					/>
-					<Button
-						isPrimary
-						disabled={
-							isSavingLayout || newLayoutName.length === 0
-						}
-						onClick={ handleSaveAsLayout }
-					>
-						{ __( 'Save', 'rrze-newsletter' ) }
-					</Button>
-					<Button
-						isSecondary
-						onClick={ () => setIsManageModalVisible( null ) }
-					>
-						{ __( 'Cancel', 'rrze-newsletter' ) }
-					</Button>
-				</Modal>
-			) }
+            <Button
+                isSecondary
+                isLink
+                isDestructive
+                onClick={() => setWarningModalVisible(true)}
+            >
+                {__("Reset newsletter layout", "rrze-newsletter")}
+            </Button>
 
-			{ warningModalVisible && (
-				<Modal
-					className="rrze-newsletter__modal"
-					title={ __(
-						'Overwrite newsletter content?',
-						'rrze-newsletter'
-					) }
-					onRequestClose={ () => setWarningModalVisible( false ) }
-				>
-					<p>
-						{ __(
-							"Changing the newsletter's layout will remove any customizations or edits you have already made.",
-							'rrze-newsletter'
-						) }
-					</p>
-					<Button
-						isPrimary
-						onClick={ () => {
-							clearPost();
-							saveLayoutIdMeta( -1 );
-							setWarningModalVisible( false );
-						} }
-					>
-						{ __( 'Reset layout', 'rrze-newsletter' ) }
-					</Button>
-					<Button
-						isSecondary
-						onClick={ () => setWarningModalVisible( false ) }
-					>
-						{ __( 'Cancel', 'rrze-newsletter' ) }
-					</Button>
-				</Modal>
-			) }
-		</Fragment>
-	);
-} );
+            {isManageModalVisible && (
+                <Modal
+                    className="rrze-newsletter__modal"
+                    title={__("Save newsletter as a layout", "rrze-newsletter")}
+                    onRequestClose={() => setIsManageModalVisible(null)}
+                >
+                    <TextControl
+                        label={__("Title", "rrze-newsletter")}
+                        disabled={isSavingLayout}
+                        value={newLayoutName}
+                        onChange={setNewLayoutName}
+                    />
+                    <Button
+                        isPrimary
+                        disabled={isSavingLayout || newLayoutName.length === 0}
+                        onClick={handleSaveAsLayout}
+                    >
+                        {__("Save", "rrze-newsletter")}
+                    </Button>
+                    <Button
+                        isSecondary
+                        onClick={() => setIsManageModalVisible(null)}
+                    >
+                        {__("Cancel", "rrze-newsletter")}
+                    </Button>
+                </Modal>
+            )}
+
+            {warningModalVisible && (
+                <Modal
+                    className="rrze-newsletter__modal"
+                    title={__(
+                        "Overwrite newsletter content?",
+                        "rrze-newsletter"
+                    )}
+                    onRequestClose={() => setWarningModalVisible(false)}
+                >
+                    <p>
+                        {__(
+                            "Changing the newsletter's layout will remove any customizations or edits you have already made.",
+                            "rrze-newsletter"
+                        )}
+                    </p>
+                    <Button
+                        isPrimary
+                        onClick={() => {
+                            clearPost();
+                            saveLayoutIdMeta(-1);
+                            setWarningModalVisible(false);
+                        }}
+                    >
+                        {__("Reset layout", "rrze-newsletter")}
+                    </Button>
+                    <Button
+                        isSecondary
+                        onClick={() => setWarningModalVisible(false)}
+                    >
+                        {__("Cancel", "rrze-newsletter")}
+                    </Button>
+                </Modal>
+            )}
+        </Fragment>
+    );
+});
