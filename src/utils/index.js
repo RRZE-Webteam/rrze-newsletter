715c259738bbc8b64ea8b6efb3ec03923f138c31
Rolf <rolf.v.d.forst@fau.de>
2024-08-07 16:27:56 +0200

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,25 @@
 /**
  * IntPluginernal dependencies
  */
-import { LAYOUT_CPT_SLUG } from './consts';
+import { LAYOUT_CPT_SLUG } from "./consts";
 
-export const isUserDefinedLayout = ( layout ) =>
-	layout && layout.post_type === LAYOUT_CPT_SLUG;
+export const isUserDefinedLayout = (layout) =>
+    layout && layout.post_type === LAYOUT_CPT_SLUG;
+
+export const getBaseUrl = () => {
+    const { protocol, hostname, port } = window.location;
+    return `${protocol}//${hostname}${port ? `:${port}` : ""}`;
+};
+
+export const convertRelativeUrlsToAbsolute = (content, baseUrl) => {
+    if (typeof content === "string") {
+        const urlPattern = /(src|href)="([^"]*)"/g;
+        return content.replace(urlPattern, (match, attr, url) => {
+            if (!url.startsWith("http")) {
+                return `${attr}="${baseUrl}${url}"`;
+            }
+            return match;
+        });
+    }
+    return "";
+};
