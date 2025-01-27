cdf8dcb18dd177145487a613125fdfcd7d9a801e
Rolf <rolf.v.d.forst@fau.de>
2025-01-27 10:11:44 +0100

    Update rrze-newsletter.php

--- rrze-newsletter.php
+++ rrze-newsletter.php
@@ -1,18 +1,18 @@
 <?php
 
 /*
-Plugin Name:       RRZE Newsletter
-Plugin URI:        https://github.com/RRZE-Webteam/rrze-newsletter
-Description:       Plugin for creating and sending HTML Newsletters.
-Version:           3.2.7
-Requires at least: 6.6
-Requires PHP:      8.2
-Author:            RRZE-Webteam
-Author URI:        https://blogs.fau.de/webworking/
-License:           GNU General Public License v2
-License URI:       http://www.gnu.org/licenses/gpl-2.0.html
-Domain Path:       /languages
-Text Domain:       rrze-newsletter
+Plugin Name:        RRZE Newsletter
+Plugin URI:         https://github.com/RRZE-Webteam/rrze-newsletter
+Version:            3.2.8
+Description:        Plugin for creating and sending HTML Newsletters.
+Author:             RRZE Webteam
+Author URI:         https://www.rrze.fau.de
+License:            GNU General Public License Version 3
+License URI:        https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:        rrze-newsletter
+Domain Path:        /languages
+Requires at least:  6.7
+Requires PHP:       8.2
 */
 
 namespace RRZE\Newsletter;
@@ -22,9 +22,6 @@ defined('ABSPATH') || exit;
 use RRZE\Newsletter\CPT\Newsletter;
 use RRZE\Newsletter\CPT\NewsletterLayout;
 use RRZE\Newsletter\CPT\NewsletterQueue;
-
-const RRZE_PHP_VERSION = '8.2';
-const RRZE_WP_VERSION = '6.6';
 
 // Load the settings config file.
 require_once 'config/settings.php';
@@ -37,61 +34,23 @@ register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
 
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
 
-/**
- * loadTextdomain
- */
-function loadTextdomain()
-{
-    load_plugin_textdomain(
-        'rrze-newsletter',
-        false,
-        sprintf('%s/languages/', dirname(plugin_basename(__FILE__)))
-    );
-}
-
-/**
- * System requirements verification.
- * @return string Return an error message.
- */
-function systemRequirements(): string
-{
-    global $wp_version;
-    // Strip off any -alpha, -RC, -beta, -src suffixes.
-    list($wpVersion) = explode('-', $wp_version);
-    $phpVersion = phpversion();
-    $error = '';
-    if (!is_php_version_compatible(RRZE_PHP_VERSION)) {
-        $error = sprintf(
-            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
-            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'rrze-newsletter'),
-            $phpVersion,
-            RRZE_PHP_VERSION
-        );
-    } elseif (!is_wp_version_compatible(RRZE_WP_VERSION)) {
-        $error = sprintf(
-            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
-            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'rrze-newsletter'),
-            $wpVersion,
-            RRZE_WP_VERSION
-        );
-    }
-    return $error;
-}
+// Load the plugin's text domain for localization.
+add_action('init', fn() => load_plugin_textdomain('rrze-newsletter', false, dirname(plugin_basename(__FILE__)) . '/languages'));
 
 /**
  * Activation callback function.
+ * @param $networkWide boolean
  */
-function activation()
+function activation($networkWide)
 {
-    loadTextdomain();
-    if ($error = systemRequirements()) {
+    if ($networkWide) {
         deactivate_plugins(plugin_basename(__FILE__));
         wp_die(
             sprintf(
                 /* translators: 1: The plugin name, 2: The error string. */
                 __('Plugins: %1$s: %2$s', 'rrze-newsletter'),
                 plugin_basename(__FILE__),
-                $error
+                __('This plugin can not be activated networkwide', 'rrze-newsletter')
             )
         );
     }
@@ -138,33 +97,90 @@ function plugin()
 }
 
 /**
- * Execute on 'plugins_loaded' API/action.
- * @return void
+ * Check system requirements for the plugin.
+ *
+ * This method checks if the server environment meets the minimum WordPress and PHP version requirements
+ * for the plugin to function properly.
+ *
+ * @return string An error message string if requirements are not met, or an empty string if requirements are satisfied.
+ */
+function systemRequirements(): string
+{
+    // Get the global WordPress version.
+    global $wp_version;
+
+    // Get the PHP version.
+    $phpVersion = phpversion();
+
+    // Initialize an error message string.
+    $error = '';
+
+    // Check if the WordPress version is compatible with the plugin's requirement.
+    if (!is_wp_version_compatible(plugin()->getRequiresWP())) {
+        $error = sprintf(
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-newsletter'),
+            $wp_version,
+            plugin()->getRequiresWP()
+        );
+    } elseif (!is_php_version_compatible(plugin()->getRequiresPHP())) {
+        // Check if the PHP version is compatible with the plugin's requirement.
+        $error = sprintf(
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-newsletter'),
+            $phpVersion,
+            plugin()->getRequiresPHP()
+        );
+    }
+
+    // Return the error message string, which will be empty if requirements are satisfied.
+    return $error;
+}
+
+/**
+ * Handle the loading of the plugin.
+ *
+ * This function is responsible for initializing the plugin, loading text domains for localization,
+ * checking system requirements, and displaying error notices if necessary.
  */
 function loaded()
 {
-    loadTextdomain();
+    // Trigger the 'loaded' method of the main plugin instance.
     plugin()->loaded();
-    if ($error = systemRequirements()) {
-        add_action('admin_init', function () use ($error) {
+    // Check system requirements.
+    if (systemRequirements()) {
+        // If there is an error, add an action to display an admin notice with the error message.
+        add_action('admin_init', function () {
+            $error = systemRequirements();
+            // Check if the current user has the capability to activate plugins.
             if (current_user_can('activate_plugins')) {
-                $pluginData = get_plugin_data(plugin()->getFile());
-                $pluginName = $pluginData['Name'];
+                // Get plugin data to retrieve the plugin's name.
+                $pluginName = plugin()->getName();
+
+                // Determine the admin notice tag based on network-wide activation.
                 $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+
+                // Add an action to display the admin notice.
                 add_action($tag, function () use ($pluginName, $error) {
                     printf(
                         '<div class="notice notice-error"><p>' .
                             /* translators: 1: The plugin name, 2: The error string. */
-                            __('Plugins: %1$s: %2$s', 'rrze-newsletter') .
+                            esc_html__('Plugins: %1$s: %2$s', 'rrze-legal') .
                             '</p></div>',
-                        esc_html($pluginName),
-                        esc_html($error)
+                        $pluginName,
+                        $error
                     );
                 });
             }
         });
+
+        // Return to prevent further initialization if there is an error.
         return;
     }
+
+    // Try to update first...
     Update::loaded();
+
+    // If there are no errors, create an instance of the 'Main' class
     new Main;
 }
