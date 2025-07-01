f72d09b9ee0c1c83049d9839a580500ab9433f47
Rolf <rolf.v.d.forst@fau.de>
2025-07-01 13:53:38 +0200

    Update rrze-newsletter.php

--- rrze-newsletter.php
+++ rrze-newsletter.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        RRZE Newsletter
 Plugin URI:         https://github.com/RRZE-Webteam/rrze-newsletter
-Version:            3.2.8
+Version:            3.2.9
 Description:        Plugin for creating and sending HTML Newsletters.
 Author:             RRZE Webteam
 Author URI:         https://www.rrze.fau.de
@@ -11,7 +11,7 @@ License:            GNU General Public License Version 3
 License URI:        https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain:        rrze-newsletter
 Domain Path:        /languages
-Requires at least:  6.7
+Requires at least:  6.8
 Requires PHP:       8.2
 */
 
