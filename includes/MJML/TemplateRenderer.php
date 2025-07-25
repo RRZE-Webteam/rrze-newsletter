027af94fc03960f771190c4d9b2bdd681a166a4e
Rolf <rolf.v.d.forst@fau.de>
2025-07-25 15:48:45 +0200

    Create TemplateRenderer.php

---
+++ includes/MJML/TemplateRenderer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RRZE\Newsletter\MJML;
+
+defined('ABSPATH') || exit;
+
+use RRZE\Newsletter\Templates;
+
+class TemplateRenderer
+{
+    /**
+     * Render the MJML template with the provided data
+     * 
+     * @param array $data Data to be used in the template
+     * @return string Rendered MJML template
+     */
+    public static function renderTemplate(array $data): string
+    {
+        $tpl = preg_replace('/\s+/', ' ', Templates::getContent('newsletter.mjml', $data));
+        // Remove a:hover styles, as they are not supported by MJML.
+        $tpl = preg_replace('/a:hover\s*{[^}]*}/i', '', $tpl);
+        return str_replace(PHP_EOL, '', $tpl);
+    }
+}
